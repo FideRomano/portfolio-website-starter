9f13e238523be1cd0c6f71d658079be428d42a83
Fidel Romano <fidel.romano.2016@hotmail.com>
2024-04-25 20:08:47 -0300

    feature: prevent screenshoot: block screen on blur block screen on keyup print

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // import components
 import Header from "./components/Header";
@@ -9,20 +9,77 @@ import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Hero />
+  const [showContent, setShowContent] = useState(true);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-      <About/>
-      <Skills/>
-      <Portfolio/>
-      <Contact/>
-      <Footer/>
-      {/* <div style={{ height: "2000px", border: "3px solid blue" }}></div> */}
-    </div>
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+  useEffect(() => {
+    const handleScreenCapture = (event) => {
+      event.preventDefault();
+      setShowContent(false);
+      // openModal();
+    };
+    document.addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "hidden") {
+        setShowContent(false);
+      } else {
+        setShowContent(true);
+      }
+    });
+    window.addEventListener("blur", () => {
+      setShowContent(false);
+    });
+    window.addEventListener("focus", () => {
+      setShowContent(true);
+    });
+    document.addEventListener("keyup", handleScreenCapture);
+    document.addEventListener("copy", handleScreenCapture);
+    document.addEventListener("paste", handleScreenCapture);
+
+    return () => {
+      document.removeEventListener("keyup", handleScreenCapture);
+    };
+  }, []);
+
+  return (
+    <>
+      {showContent ? (
+        <div>
+          <Header />
+          <Hero />
+
+          <About />
+          <Skills />
+          <Portfolio />
+          <Contact />
+          <Footer />
+          {/* <div style={{ height: "2000px", border: "3px solid blue" }}></div> */}
+        </div>
+      ) : (
+        <div
+          style={{ backgroundColor: "black", height: "100vh", width: "100vw" }}
+        >
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+          >
+            <h2>Modal Title</h2>
+            <p>This is the modal content.</p>
+            <button onClick={closeModal}>Close Modal</button>
+          </Modal>
+        </div>
+      )}
+    </>
   );
 };
 
